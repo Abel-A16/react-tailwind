7f51874b52d8d31f63bacc8671b2eb2d4e347cf7
Abel-A16 <zerubabelendale16@gmail.com>
2024-10-10 12:26:30 +0300

    navbar component created

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div>
+      <div className='logo'>
+        <img src='' alt=''/>
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
